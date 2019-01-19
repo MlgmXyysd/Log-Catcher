0682e16b9a02bf825b42315afe7449b6a78f800c
Jim Wu <mlgmxyysd@meowcat.org>
2019-01-19 18:25:20 +0800

    Add files via upload

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 time=$(date +%Y%m%d.%H%M%S)
+mkdir -p /data/media/0/logs
 echo "--------- Bootloop log catched">>/data/media/0/logs/$time.log
 echo "--------- LogCat Module by MlgmXyysd">>/data/media/0/logs/$time.log
 echo "--------- QQ chat group 855219808">>/data/media/0/logs/$time.log
