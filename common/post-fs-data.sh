c50cdc0920d31d7e146552510c014dc9e2f57bcf
MlgmXyysd <mlgmxyysd@meowcat.org>
2019-01-20 10:55:20 +0800

    v6

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 time=$(date +%Y%m%d.%H%M%S)
-mkdir -p /data/media/0/logs
-echo "--------- Bootloop log catched">>/data/media/0/logs/$time.log
-echo "--------- LogCat Module by MlgmXyysd">>/data/media/0/logs/$time.log
-echo "--------- QQ chat group 855219808">>/data/media/0/logs/$time.log
-logcat>>/data/media/0/logs/$time.log &
+mkdir -p /data/local/logs
+echo "--------- Bootloop log catched">>/data/local/logs/$time.log
+echo "--------- LogCat Module by MlgmXyysd">>/data/local/logs/$time.log
+echo "--------- QQ chat group 855219808">>/data/local/logs/$time.log
+logcat>>/data/local/logs/$time.log &
