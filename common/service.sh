7b02849c8bd2ad747ec03d2d01d9551bfa2bcd4b
Jim Wu <mlgmxyysd@meowcat.org>
2019-02-19 00:47:08 +0800

    v15

--- common/service.sh
+++ common/service.sh
@@ -1,7 +1,12 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
-file=/data/local/.logcatcher-still
+file=/data/local/logcatcher/boot.lcs
 if [ ! -f "$file" ];
 then
-  pkill -f logcatcher-boot-mlgmxyysd:V
+  pkill -f logcatcher-boot-mlgmxyysd:S
+fi
+file_err=/data/local/logcatcher/error.lcs
+if [ ! -f "$file_err" ];
+then
+  pkill -f logcatcher-boot-error-mlgmxyysd:S
 fi
