b0cf0556ff1dc41bde7857913311569ca3aa4da7
Jim Wu <mlgmxyysd@meowcat.org>
2019-02-19 00:45:39 +0800

    v15

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,5 +1,9 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+
+lc_ver="v15"
+lc_vercode=15
+
 msg[0]="Now just a little more only just a little more..."
 msg[1]="It's not a right that I'm due, my duty that is must have been kept..."
 msg[2]="Since one day you will disappear, I'll keep every part of you..."
@@ -7,11 +11,9 @@ msg[3]="Yes we are time fliers scaling the walls of time climbers, tired of play
 msg[4]="Wherever you are in the world, I'll search for you..."
 msg[5]="Mitsuha. Mitsuha. Mitsuha, your name is Mitsuha..."
 msg[6]="Someone dear to me. I don't want to forget. I shouldn't forget!"
+
 time=$(date +%Y-%m-%d-%H-%M-%S)
-path=/data/local/boot
-file=$path/boot-$time.log
-num=$(($RANDOM+100000000))
-rand=$(($num%7))
+
 android_sdk=`getprop ro.build.version.sdk`
 build_desc=`getprop ro.build.description`
 product=`getprop ro.build.product`
@@ -22,12 +24,17 @@ arch=`getprop ro.product.cpu.abi`
 device=`getprop ro.product.device`
 android=`getprop ro.build.version.release`
 build=`getprop ro.build.id`
+
+path=/data/local/boot
+file=$path/boot-$time.log
+num=$(($RANDOM+100000000))
+rand=$(($num%7))
 mkdir -p $path
 rm -rf $file
 touch $file
 echo "--------- beginning of head">>$file
 echo "Log Catcher by MlgmXyysd">>$file
-echo "Version: v14 (14)"
+echo "Version: ${lc_ver} (${lc_vercode})">>$file
 echo "QQ chat group 855219808">>$file
 echo ${msg[$rand]}>>$file
 echo "--------- beginning of system info">>$file
@@ -42,3 +49,28 @@ echo "Manufacturer: ${manufacturer}">>$file
 echo "Brand: ${brand}">>$file
 echo "Product: ${product}">>$file
 logcat -v long *:V logcatcher-boot-mlgmxyysd:S>>$file &
+
+path_err=/data/local/boot_error
+file_err=$path_err/error-$time.log
+num_err=$(($RANDOM+100000000))
+rand_err=$(($num%7))
+mkdir -p $path_err
+rm -rf $file_err
+touch $file_err
+echo "--------- beginning of head">>$file_err
+echo "Log Catcher by MlgmXyysd">>$file_err
+echo "Version: ${lc_ver} (${lc_vercode}) (Error log)">>$file_err
+echo "QQ chat group 855219808">>$file_err
+echo ${msg[$rand_err]}>>$file_err
+echo "--------- beginning of system info">>$file_err
+echo "Android version: ${android}">>$file_err
+echo "Android sdk: ${android_sdk}">>$file_err
+echo "Android build: ${build}">>$file_err
+echo "Fingerprint: ${fingerprint}">>$file_err
+echo "ROM build description: ${build_desc}">>$file_err
+echo "Architecture: ${arch}">>$file_err
+echo "Device: ${device}">>$file_err
+echo "Manufacturer: ${manufacturer}">>$file_err
+echo "Brand: ${brand}">>$file_err
+echo "Product: ${product}">>$file_err
+logcat -v long *:W logcatcher-boot-error-mlgmxyysd:S>>$file_err &
