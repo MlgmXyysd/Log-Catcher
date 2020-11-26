8e944c7526ef8621f0ccd09ab897b2130f261c57
Howard20181 <40033067+Howard20181@users.noreply.github.com>
2020-11-27 00:21:07 +0800

    Update

--- customize.sh
+++ customize.sh
@@ -1,10 +1,5 @@
-REPLACE="
-"
-
 set_permissions() {
   set_perm_recursive  $MODPATH  0  0  0755  0755
 }
 
 SKIPUNZIP=1
-unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
-. $TMPDIR/functions.sh
--- post-fs-data.sh
+++ post-fs-data.sh
@@ -4,6 +4,14 @@ MODDIR=${0%/*}
 lc_ver="v18"
 lc_vercode=18
 
+msg[0]="Now just a little more only just a little more..."
+msg[1]="It's not a right that I'm due, my duty that is must have been kept..."
+msg[2]="Since one day you will disappear, I'll keep every part of you..."
+msg[3]="Yes we are time fliers scaling the walls of time climbers, tired of playing hide and seek with time..."
+msg[4]="Wherever you are in the world, I'll search for you..."
+msg[5]="Mitsuha. Mitsuha. Mitsuha, your name is Mitsuha..."
+msg[6]="Someone dear to me. I don't want to forget. I shouldn't forget!"
+
 time=$(date +%Y-%m-%d-%H-%M-%S)
 
 android_sdk=`getprop ro.build.version.sdk`
@@ -27,6 +35,7 @@ touch $file
 echo "--------- beginning of head">>$file
 echo "Log Catcher by MlgmXyysd">>$file
 echo "Version: ${lc_ver} (${lc_vercode})">>$file
+echo ${msg[$rand]}>>$file
 echo "--------- beginning of system info">>$file
 echo "Android version: ${android}">>$file
 echo "Android sdk: ${android_sdk}">>$file
@@ -50,6 +59,7 @@ touch $file_err
 echo "--------- beginning of head">>$file_err
 echo "Log Catcher by MlgmXyysd">>$file_err
 echo "Version: ${lc_ver} (${lc_vercode}) (Error log)">>$file_err
+echo ${msg[$rand_err]}>>$file_err
 echo "--------- beginning of system info">>$file_err
 echo "Android version: ${android}">>$file_err
 echo "Android sdk: ${android_sdk}">>$file_err
